1714ca7a4c3630984e2df7d1719ae0f4e7c25a49
PrimalSword <106413088+PrimalSword@users.noreply.github.com>
2026-07-30 13:19:53 -0300

    mobile: configure app module

---
+++ mobile/android/app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.orbis.watchlab"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.orbis.watchlab"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
